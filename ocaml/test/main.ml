0f42758c22e89c1f5832811829f008b4415a3ae3
Christopher Fujino <christopherfujino@gmail.com>
2025-05-13 20:35:51 -0700

    cleanup

--- ocaml/test/main.ml
+++ ocaml/test/main.ml
@@ -1,4 +1,6 @@
 open OUnit2
 
-let tests = [ ("todo" >:: fun _ -> assert_equal true true) ]
-let () = run_test_tt_main ("suite" >::: tests)
+let parser_tests =
+  "parser" >::: [ ("todo" >:: fun _ -> assert_equal true true) ]
+
+let () = run_test_tt_main parser_tests
