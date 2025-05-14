a3cdc31c03d4750c4cd8ada71019b0dc07200bae
Christopher Fujino <christopherfujino@gmail.com>
2025-05-13 22:39:47 -0700

    improve debugging

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -4,7 +4,7 @@ let rec repl () =
   (* %! means flush *)
   Printf.printf "> %!";
   let line = read_line () in
-  let ast = Lexing.from_string line |> Parser.prog Lexer.read in
+  let ast = Compiler.parse_line line in
   Ast.stmt_to_str ast |> print_endline;
   repl ()
 
--- ocaml/test/main.ml
+++ ocaml/test/main.ml
@@ -4,12 +4,13 @@ open Sloth_script
 let parser_tests =
   let make_test name program expectation =
     name >:: fun _ ->
-    let stmt = Lexing.from_string program |> Parser.prog Lexer.read in
+    let stmt = Compiler.parse_line program in
     assert_equal ~printer:(fun s -> s) expectation (Ast.stmt_to_str stmt)
   in
-  "parser" >::: [
-    make_test "num literal" "11" "(ExprStmt 11)";
-    make_test "addition" "1 + 1" "TODO";
-  ]
+  "parser"
+  >::: [
+         make_test "num literal" "11" "(ExprStmt 11)";
+         make_test "addition" "1 + 1" "TODO";
+       ]
 
 let () = run_test_tt_main parser_tests
