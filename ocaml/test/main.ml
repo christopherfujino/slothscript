9fca5ab25d9088a7bf4c90b7d68ab766cfc2dcc1
Christopher Fujino <christopherfujino@gmail.com>
2025-05-13 20:42:06 -0700

    add failing test

--- ocaml/test/main.ml
+++ ocaml/test/main.ml
@@ -1,6 +1,12 @@
 open OUnit2
+open Sloth_script
 
 let parser_tests =
-  "parser" >::: [ ("todo" >:: fun _ -> assert_equal true true) ]
+  "parser"
+  >::: [
+         ( "todo" >:: fun _ ->
+           let _ = Lexing.from_string "11" |> Parser.prog Lexer.read in
+           () );
+       ]
 
 let () = run_test_tt_main parser_tests
