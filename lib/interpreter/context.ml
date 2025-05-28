c73c903adb92a1a632d6faa598a377b057b7c349
Christopher Fujino <christopherfujino@gmail.com>
2025-05-27 22:06:39 -0700

    implement test stdlib

--- lib/interpreter/context.ml
+++ lib/interpreter/context.ml
@@ -1,12 +1,29 @@
-(* Context *)
-type t = { l : (module Sloth_stdlib.StdlibSig) }
+module Identifiers = struct
+  type t = (string, Runtime.t) Hashtbl.t list ref
 
-let make_prod_ctx () : t = { l = (module Sloth_stdlib.Prod) }
+  let create () = ref [ Hashtbl.create 8 ]
+
+  let set (ids : t) id v =
+    let tbl = List.hd !ids in
+    let maybe_val = Hashtbl.find_opt tbl id in
+    match maybe_val with
+    | Some _ ->
+        let msg = Printf.sprintf "cannot rebind name %s" id in
+        failwith msg
+    | None -> Hashtbl.add tbl id v
+end
+
+(* Context *)
+type t = { l : (module Sloth_stdlib.StdlibSig); identifiers : Identifiers.t }
+
+let make_ctx m = { l = m; identifiers = Identifiers.create () }
 
 let rec interpret_stmt ctx stmt =
   let open Compiler.Ast in
   match stmt with
-  | LetStmt (_, _) -> (* TODO *) failwith "Todo"
+  | LetStmt (id, e) ->
+      let v = interpret_expr ctx e in
+      Identifiers.set ctx.identifiers id v
   | ExprStmt expr ->
       let v = interpret_expr ctx expr in
       let module L = (val ctx.l) in
