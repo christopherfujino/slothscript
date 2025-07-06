ecf90c197441eecaf24ea095875b723916042c35
Christopher Fujino <christopherfujino@gmail.com>
2025-07-05 17:38:55 -0700

    clean

--- test/train.ml
+++ test/train.ml
@@ -5,14 +5,7 @@ let () =
   let spec_results =
     find_child_specs "./green_specs"
     |> List.map ~f:Spec_parser.deserialize
-    |> List.map ~f:(function
-         | Ok spec ->
-             Printf.printf "%s => %s\n" spec.name spec.ast;
-             None
-         | Error reason -> Some reason)
   in
-  let errors =
-    List.filter spec_results ~f:(function None -> false | Some _ -> true)
-  in
-  if List.length errors > 0 then
-    List.iter errors ~f:(fun opt -> Option.value_exn opt |> print_endline)
+  let _, bad_specs = List.partition_result spec_results in
+  if List.length bad_specs > 0 then
+    List.iter bad_specs ~f:(print_endline)
