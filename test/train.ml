0ce6fefedb52e8a07d6484b10a80b23fcecae467
Christopher Fujino <christopherfujino@gmail.com>
2025-07-10 13:37:31 -0700

    test/train only write if ast is wrong, and tell whether it was ugly or wrong

--- test/train.ml
+++ test/train.ml
@@ -1,6 +1,37 @@
 open Common
 open Core
 
+let f spec_dir i arg =
+  if i = 0 then
+    (* args[0] is the bin name *)
+    ()
+  else
+    let spec = Spec_parser.deserialize arg in
+    let _, prog =
+      (Compiler.Environment.create ()
+      |> Compiler.Stdlib_stubs.populate |> Compiler.Main.parse)
+      @@ spec.program
+    in
+    let canonical_ast =
+      Compiler.Optimizer.prog_to_str prog |> Printer.sexp_formatter
+    in
+    let pretty_ast = Printer.sexp_formatter spec.ast in
+    let state =
+      if String.equal canonical_ast spec.ast then `Good
+      else if String.equal canonical_ast pretty_ast then `Ugly
+      else `Bad
+    in
+    let src_path = Printf.sprintf "%s/%s" spec_dir arg in
+    (match state with
+    | `Good -> ()
+    | `Bad ->
+        Printf.printf "[Bad]  %s\n" arg;
+        Spec_parser.serialize src_path { spec with ast = pretty_ast }
+    | `Ugly ->
+        Printf.printf "[Ugly] %s\n" arg;
+        Spec_parser.serialize src_path { spec with ast = pretty_ast });
+    ()
+
 let () =
   let spec_dir =
     match Sys.getenv "TEST_DIR" with
@@ -9,14 +40,4 @@ let () =
   in
   let args = Sys.get_argv () in
   if Array.length args = 1 then failwith "Usage: train.exe [SPEC_PATH]+";
-  Array.iteri args ~f:(fun i arg ->
-      print_endline arg;
-      if i = 0 then
-        (* args[0] is the bin name *)
-        ()
-      else
-        let spec = Spec_parser.deserialize arg in
-        let pretty_ast = Printer.sexp_formatter spec.ast in
-        if not (String.equal pretty_ast spec.ast) then
-          let src_path = Printf.sprintf "%s/%s" spec_dir arg in
-          Spec_parser.serialize src_path { spec with ast = pretty_ast })
+  Array.iteri args ~f:(f spec_dir)
