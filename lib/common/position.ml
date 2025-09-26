ae68b196474fda2dbab1fb95dbd3b10008d04096
Christopher Fujino <christopherfujino@gmail.com>
2025-09-26 13:53:28 -0700

    remove commented code

--- lib/common/position.ml
+++ lib/common/position.ml
@@ -1,17 +1,5 @@
 open Core
 
-(*
-type t = { pos_fname : string; pos_lnum : int; pos_bol : int; pos_cnum : int }
-
-let t_of_lexing_position (p : Lexing.position) : t =
-  {
-    pos_fname = p.pos_fname;
-    pos_lnum = p.pos_lnum;
-    pos_bol = p.pos_bol;
-    pos_cnum = p.pos_cnum;
-  }
-*)
-
 let rec indent_str b i s =
   if i <= 0 then Buffer.add_string b s
   else (
