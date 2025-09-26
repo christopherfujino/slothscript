1eac04aae79945ed0efff8161acad5ff46a7fed7
Christopher Fujino <christopherfujino@gmail.com>
2025-09-26 12:53:48 -0700

    make positions opaque

--- lib/compiler/optimizer.mli
+++ lib/compiler/optimizer.mli
@@ -1,72 +1,72 @@
 type decl = private
   | FuncDecl of {
       name : string;
-      parameters : (string * Sloth_common.Position.t) list;
+      parameters : (string * (Sloth_common.Position.t[@sexp.opaque])) list;
       block : stmt list;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
   | StmtDecl of stmt
 [@@deriving sexp]
 
 and stmt =
   | ExprStmt of expr
-  | BreakingStmt of Ast.breaking_type * expr option * Sloth_common.Position.t
+  | BreakingStmt of Ast.breaking_type * expr option * (Sloth_common.Position.t[@sexp.opaque])
 [@@deriving sexp]
 
 and expr =
-  | Num of float * Sloth_common.Position.t
-  | Bool of bool * Sloth_common.Position.t
-  | Null of Sloth_common.Position.t
-  | String of string_parts list * Sloth_common.Position.t
-  | List of expr list * Sloth_common.Position.t
-  | HashMap of (expr * expr) list * Sloth_common.Position.t
-  | Subscript of expr * expr * Sloth_common.Position.t
-  | IdRef of string * Sloth_common.Position.t
-  | ContextId of string * Sloth_common.Position.t
-  | Equality of expr * expr * bool * Sloth_common.Position.t
-  | Binary of expr * expr * Ast.operator * Sloth_common.Position.t
-  | FuncInvoc of expr * expr list * Sloth_common.Position.t
+  | Num of float * (Sloth_common.Position.t[@sexp.opaque])
+  | Bool of bool * (Sloth_common.Position.t[@sexp.opaque])
+  | Null of (Sloth_common.Position.t[@sexp.opaque])
+  | String of string_parts list * (Sloth_common.Position.t[@sexp.opaque])
+  | List of expr list * (Sloth_common.Position.t[@sexp.opaque])
+  | HashMap of (expr * expr) list * (Sloth_common.Position.t[@sexp.opaque])
+  | Subscript of expr * expr * (Sloth_common.Position.t[@sexp.opaque])
+  | IdRef of string * (Sloth_common.Position.t[@sexp.opaque])
+  | ContextId of string * (Sloth_common.Position.t[@sexp.opaque])
+  | Equality of expr * expr * bool * (Sloth_common.Position.t[@sexp.opaque])
+  | Binary of expr * expr * Ast.operator * (Sloth_common.Position.t[@sexp.opaque])
+  | FuncInvoc of expr * expr list * (Sloth_common.Position.t[@sexp.opaque])
   | MethodInvoc of {
       receiver : expr;
       target : string;
       args : expr list;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
   | FuncExpr of {
-      parameters : (string * Sloth_common.Position.t) list;
+      parameters : (string * (Sloth_common.Position.t[@sexp.opaque])) list;
       block : stmt list;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
-  | IfExpr of cond_cont * Sloth_common.Position.t
+  | IfExpr of cond_cont * (Sloth_common.Position.t[@sexp.opaque])
   | UnaryExpr of {
       target : expr;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
       operator : Ast.operator;
     }
-  | DoBlock of stmt list * Sloth_common.Position.t
-  | ObjDeref of expr * string * Sloth_common.Position.t
-  | LetExpr of string * expr * Sloth_common.Position.t
-  | AssignExpr of string * expr * Sloth_common.Position.t
+  | DoBlock of stmt list * (Sloth_common.Position.t[@sexp.opaque])
+  | ObjDeref of expr * string * (Sloth_common.Position.t[@sexp.opaque])
+  | LetExpr of string * expr * (Sloth_common.Position.t[@sexp.opaque])
+  | AssignExpr of string * expr * (Sloth_common.Position.t[@sexp.opaque])
   | SubAssignExpr of {
       subscript : expr;
       value : expr;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
-  | ForLoop of expr * expr * expr * stmt list * Sloth_common.Position.t
+  | ForLoop of expr * expr * expr * stmt list * (Sloth_common.Position.t[@sexp.opaque])
   | ForInLoop of {
       iterator_name : string;
       iteratee : expr;
       block : stmt list;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
-  | WithExpr of (string * expr) list * stmt list * Sloth_common.Position.t
+  | WithExpr of (string * expr) list * stmt list * (Sloth_common.Position.t[@sexp.opaque])
 [@@deriving sexp]
 
 and string_parts =
-  | FullString of string * Sloth_common.Position.t
-  | StartStringInterp of string * Sloth_common.Position.t
-  | MiddleStringInterp of string * Sloth_common.Position.t
-  | EndStringInterp of string * Sloth_common.Position.t
+  | FullString of string * (Sloth_common.Position.t[@sexp.opaque])
+  | StartStringInterp of string * (Sloth_common.Position.t[@sexp.opaque])
+  | MiddleStringInterp of string * (Sloth_common.Position.t[@sexp.opaque])
+  | EndStringInterp of string * (Sloth_common.Position.t[@sexp.opaque])
   | ExpressionStringInterp of expr
 [@@deriving sexp]
 
@@ -75,9 +75,9 @@ and cond_cont = private
       conditional : expr;
       block : stmt list;
       continuation : cond_cont option;
-      pos : Sloth_common.Position.t;
+      pos : (Sloth_common.Position.t[@sexp.opaque]);
     }
-  | ElseCont of stmt list * Sloth_common.Position.t
+  | ElseCont of stmt list * (Sloth_common.Position.t[@sexp.opaque])
 [@@deriving sexp]
 
 val optimize_prog : Environment.t -> Ast.decl list -> Environment.t * decl list
